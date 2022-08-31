c34ec04c1e7a3170867abb61d6e4ef17a92864a3
cryptic-wizard <87053379+cryptic-wizard@users.noreply.github.com>
2022-08-30 21:55:44 -0500

    Upgraded to dotnet 6.0, added test fixture

---
+++ HexConverterTest/Drivers/TestFixture.cs
@@ -0,0 +1,21 @@
+﻿using CrypticWizard.HexConverter;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace HexConverterTest.Drivers
+{
+    public class TestFixture
+    {
+        public Stopwatch sw = new();
+        public long LinqMillis = 0;
+        public long HexConverterMillis = 0;
+
+        public byte[] ByteArray;
+        public bool ExceptionThrown = false;
+        public string Hex;
+        public string[] HexArray;
+        public List<string> HexList;
+    }
+}
---
+++ HexConverterTest/Steps/TestFixtureSteps.cs
@@ -0,0 +1,29 @@
+﻿using BoDi;
+using TechTalk.SpecFlow;
+using HexConverterTest.Drivers;
+
+namespace CrypticWizard.HexConverter
+{
+    [Binding]
+    public class TestFixtureSteps
+    {
+        private readonly IObjectContainer objectContainer;
+
+        public TestFixtureSteps(IObjectContainer objectContainer)
+        {
+            this.objectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            objectContainer.RegisterInstanceAs(new TestFixture());
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+
+        }
+    }
+}
